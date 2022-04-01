fbf35f5a9fde05a5fd99020db1338148ad01d7f5
Hidayah Rizki Ramadhan <58053633+hidayahrr@users.noreply.github.com>
2022-04-01 17:59:58 +0700

    Add files via upload

---
+++ calculator-basic.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter number here: "))
+operation = input("Enter operator here: ")
+num2 = float(input("Enter number here: "))
+
+if operation == "+":
+    print(num1 + num2)
+elif operation == "-":
+    print(num1 - num2)
+elif operation == "/":
+    print(num1 / num2)
+elif operation == "*":
+    print(num1 * num2)
+else:
+    print("error")
